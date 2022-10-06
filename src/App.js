7f9992259239acde0a31f359f63aaa2446778d51
mhira-codes <alhassansamiratu991@gmail.com>
2022-10-06 10:06:43 +0000

    changes in file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Hero from './components/Hero/Hero';
+import Program from './components/Hero/Programs/Program';
 
 function App() {
   return (
     <div className="App">
       <Hero/>
+      <Program/>
     </div>
   );
 }
