f3cf77aaf85cf57cf613802a7d8257b8f976b7be
mhira-codes <alhassansamiratu991@gmail.com>
2022-10-10 11:23:56 +0000

    minor changes

--- src/components/Hero/Reasons/Reason.jsx
+++ src/components/Hero/Reasons/Reason.jsx
@@ -4,6 +4,10 @@ import Img1 from "../../../assets/image1.png";
 import Img2 from "../../../assets/image2.png";
 import Img3 from "../../../assets/image3.png";
 import Img4 from "../../../assets/image4.png";
+import tick from "../../../assets/tick.png";
+import nb from "../../../assets/nb.png";
+import adidas from "../../../assets/adidas.png";
+import nike from "../../../assets/nike.png";
 const Reason = () => {
   return (
     <div className="reasons">
@@ -14,7 +18,42 @@ const Reason = () => {
         <img src={Img4} alt=" image4" />
       </div>
       <div className="right-reason">
-        fgc vln
+        <span>Some Reasons</span>
+        <div>
+          <span className="outline-text">Why</span>
+          <span> choose us?</span>
+        </div>
+        <div className="reason-details">
+          <div>
+            <img src={tick} alt="" />
+            <span>OVER +138 COACHES</span>
+          </div>
+          <div>
+            <img src={tick} alt="" />
+            <span>TRAIN FASTER AND SMATER THAN BEFORE</span>
+          </div>
+          <div>
+            <img src={tick} alt="" />
+            <span>1 FREE PROGRAM FOR A NEW MEMBER</span>
+          </div>
+          <div>
+            <img src={tick} alt="" />
+            <span>RELIABLE PARTNERS</span>
+          </div>
+        </div>
+        <span
+          style={{
+            color: "var(--gray)",
+            fontWeight: "normal",
+          }}
+        >
+          OUR PARTNERS
+        </span>
+        <div className="partners">
+          <img src={nb} alt="" />
+          <img src={adidas}alt="" />
+          <img src={nike} alt="" />
+        </div>
       </div>
     </div>
   );
