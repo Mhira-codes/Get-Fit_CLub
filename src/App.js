575d41563016a9c98bdb7c9a79167796215a1d67
mhira-codes <alhassansamiratu991@gmail.com>
2022-10-10 11:22:05 +0000

    addition of compoments

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 
 import './App.css';
 import Hero from './components/Hero/Hero';
+import Plans from './components/Hero/Plans/Plans';
 import Program from './components/Hero/Programs/Program';
 import Reasons from './components/Hero/Reasons/Reason';
+import Testimonials from './components/Hero/Testimonials/Testimonial';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Hero/>
       <Program/>
     <Reasons/>
+<Plans/>
+<Testimonials/>
     </div>
   );
 }
