4cf0d8858b2be9055ad7cdf47239f9537a13f528
mhira-codes <alhassansamiratu991@gmail.com>
2022-10-10 11:22:33 +0000

    minor changes

--- src/components/Hero/Header/Header.jsx
+++ src/components/Hero/Header/Header.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import "./Header.css"
+import logo from "../../../assets/logo.png"
 
 
 const Header = () => {
-  const  Logo = 'https://images.unsplash.com/photo-1583454110551-21f2fa2afe61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHdvcmtvdXR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
-
-  return (
+ return(
     <div className="header">
-<img src={Logo} alt="logo" className="logo"/>
+<img src={logo} alt="logo" className="logo"/>
 
 <ul className='h-menu'>
 <li>Home</li>
