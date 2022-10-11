a5bad1066141507dc809357070a19d224d25dc05
mhira-codes <alhassansamiratu991@gmail.com>
2022-10-11 18:14:31 +0000

    changes in file

--- src/components/Hero/Footer/Footer.jsx
+++ src/components/Hero/Footer/Footer.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-import "./Footer.css"
+import React from "react";
+import "./Footer.css";
+import github from "../../../assets/github.png";
+import linkedin from "../../../assets/linkedin.png";
+import instagram from "../../../assets/instagram.png";
+import logo from "../../../assets/logo.png";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer-container">
+      <hr />
+      <div className="footer-sub">
+        <div className="footer-images">
+          <img src={github} alt="" />
+          <img src={linkedin} alt="" />
+          <img src={instagram} alt="" />
+        </div>
+        <div className="footer-logo">
+          <img src={logo} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
