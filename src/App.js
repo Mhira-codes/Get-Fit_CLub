c35b96b6c3c08e6159e76ae571d21cb7d0b15648
mhira-codes <alhassansamiratu991@gmail.com>
2022-10-11 17:16:36 +0000

    changes in App.js file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-
-import './App.css';
-import Hero from './components/Hero/Hero';
-import Plans from './components/Hero/Plans/Plans';
-import Program from './components/Hero/Programs/Program';
-import Reasons from './components/Hero/Reasons/Reason';
-import Testimonials from './components/Hero/Testimonials/Testimonial';
+import "./App.css";
+import Hero from "./components/Hero/Hero";
+import Join from "./components/Hero/Join/Join";
+import Plans from "./components/Hero/Plans/Plans";
+import Program from "./components/Hero/Programs/Program";
+import Reasons from "./components/Hero/Reasons/Reason";
+import Testimonials from "./components/Hero/Testimonials/Testimonial";
+import Footer from "./components/Hero/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Hero/>
-      <Program/>
-    <Reasons/>
-<Plans/>
-<Testimonials/>
+      <Hero />
+      <Program />
+      <Reasons />
+      <Plans />
+      <Testimonials />
+      <Join />
+<Footer/>
     </div>
   );
 }
